70e550311155b28470b9cee7702896802ce4461f
MouadAbouOthmane <mouad0abouothmane@gmail.com>
2024-01-06 18:04:56 +0100

    5. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """app module"""
 
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(response_or_exc):
     """teardown appcontext"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ json 404 page """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
